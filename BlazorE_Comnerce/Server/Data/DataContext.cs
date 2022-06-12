1341d379827d3a426304592f898b7f62e81167cd
morkaleb <morkaleb@gmail.com>
2022-06-12 11:11:26 -0700

    initial

---
+++ BlazorE_Comnerce/Server/Data/DataContext.cs
@@ -0,0 +1,71 @@
+﻿namespace BlazorE_Comnerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) 
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Rumor Gatherer",
+                    ImageUrl = "https://www.demonictrader.ca/marketapi/cardGetter/get-card-image/?imageId=f8b87807-cac5-4e33-a8ed-a9ced0cd83a1",
+                    Description = "Elf Wizard",
+                    Price = .5m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Junji, the Midnight Sky",
+                    ImageUrl = "https://www.demonictrader.ca/marketapi/" +
+                    "cardGetter/get-card-image/?imageId=9033cf27-d9e6-49b8-8ee1-c1f38c9680b9",
+                    Description = "Kamigawa: Neon Dynasty",
+                    Price = 7.46m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Expressive Iteration",
+                    ImageUrl = "https://www.demonictrader.ca/marketapi/cardGetter/" +
+                    "get-card-image/?imageId=31b770cc-09e7-4c0b-b2a4-462ab4f7200d",
+                    Description = "",
+                    Price = 7.74m
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Ziatora, the Incinerator",
+                    ImageUrl = "https://www.demonictrader.ca/marketapi/cardGetter/" +
+                    "get-card-image/?imageId=ba4fa4c8-f09f-4d97-a7d1-1b93caf7d4f9",
+                    Description = "Streets of New Capenna",
+                    Price = 4.45m
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Sokenzan, Crucible of Defiance",
+                    ImageUrl = "https://www.demonictrader.ca/marketapi/cardGetter" +
+                        "/get-card-image/?imageId=a548dcd-c1dd-492d-a69f-c65dfeef0633",
+                    Description = "Kamigawa: Neon Dynasty",
+                    Price = 3.01m
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "Esper Sentinel",
+                    ImageUrl = "https://www.demonictrader.ca/marketapi/cardGetter/get-card-image" +
+                    "/?imageId=676758ee-dac8-4c97-8a62-fff25bcbb6df",
+                    Description = "Modern Horizons 2 ",
+                    Price = 32.65m
+                }
+            );     
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BlazorE_Comnerce/Server/Migrations/20220612174851_productSeeding.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorE_Comnerce.Server.Migrations
+{
+    public partial class productSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Elf Wizard", "https://www.demonictrader.ca/marketapi/cardGetter/get-card-image/?imageId=f8b87807-cac5-4e33-a8ed-a9ced0cd83a1", 0.5m, "Rumor Gatherer" },
+                    { 2, "Kamigawa: Neon Dynasty", "https://www.demonictrader.ca/marketapi/cardGetter/get-card-image/?imageId=9033cf27-d9e6-49b8-8ee1-c1f38c9680b9", 7.46m, "Junji, the Midnight Sky" },
+                    { 3, "", "https://www.demonictrader.ca/marketapi/cardGetter/get-card-image/?imageId=31b770cc-09e7-4c0b-b2a4-462ab4f7200d", 7.74m, "Expressive Iteration" },
+                    { 4, "Streets of New Capenna", "https://www.demonictrader.ca/marketapi/cardGetter/get-card-image/?imageId=ba4fa4c8-f09f-4d97-a7d1-1b93caf7d4f9", 4.45m, "Ziatora, the Incinerator" },
+                    { 5, "Kamigawa: Neon Dynasty", "https://www.demonictrader.ca/marketapi/cardGetter/get-card-image/?imageId=a548dcd-c1dd-492d-a69f-c65dfeef0633", 3.01m, "Sokenzan, Crucible of Defiance" },
+                    { 6, "Modern Horizons 2 ", "https://www.demonictrader.ca/marketapi/cardGetter/get-card-image/?imageId=676758ee-dac8-4c97-8a62-fff25bcbb6df", 32.65m, "Esper Sentinel" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
